43de0517ae61face67587712fbdd597911339789
bilal <abdurrahmankar697@gmail.com>
2022-07-07 16:56:06 +0300

    creata a game where a dog cathces ball randomly spawning

---
+++ Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    private float time = 0.0f;
+    private float timer = 2.0f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (time > timer)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                // On spacebar press, send dog
+                Instantiate(dogPrefab, new Vector3(17, 0, 0), dogPrefab.transform.rotation);
+                time = 0.0f;
+            }
+        }
+    }
+}
