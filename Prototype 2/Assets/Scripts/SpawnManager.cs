918f691c0ed1e5d38503430ab6d95b1b51be923d
bilal <abdurrahmankar697@gmail.com>
2022-07-07 15:58:21 +0300

    structure the code better

--- Prototype 2/Assets/Scripts/SpawnManager.cs
+++ Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
+    public float spawnRangeX = 20;
+    public float spawnPosZ = 20;
 
     // Start is called before the first frame update
     void Start()
@@ -17,8 +19,17 @@ public class SpawnManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.S))
         {
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 20), animalPrefabs[animalIndex].transform.rotation);
+            SpawnRandomANimal();
         }
+    }
+
+    void SpawnRandomANimal()
+    {
+        //randomise the spawn location
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+
+        //randomise the animal type
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
     }
 }
